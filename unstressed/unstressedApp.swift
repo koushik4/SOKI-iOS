0498d0a98ff642a41ef58482cc47b1334d33baf9
srinivaskoushik <srinivas.koushik4@gmail.com>
2024-04-18 14:37:13 -0400

    Initial Commit

---
+++ unstressed/unstressedApp.swift
@@ -0,0 +1,17 @@
+//
+//  unstressedApp.swift
+//  unstressed
+//
+//  Created by Srinivas Koushik on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct unstressedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
